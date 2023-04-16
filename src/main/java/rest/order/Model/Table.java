bd9003e95510408f7ee723e03114ca7b369e33f4
choi4624 <choi4624@gmail.com>
2023-04-16 23:31:06 +0900

    테이블이 일종의 큐 처럼 작동할 것 같습니다.

--- src/main/java/rest/order/Model/Table.java
+++ src/main/java/rest/order/Model/Table.java
@@ -10,10 +10,14 @@ public class Table {
     // need to do 
     String locationString;
 
-    public Table(int tableNO, int seats, String locationString) {
+    // 예약 대기 리스트 지원에 따라 테이블 큐를 넣어서 순서대로 넣고 빠지는 형태의 메소드 구현이 필요해 보입니다. 
+    int tableQue;
+
+    public Table(int tableNO, int seats, String locationString, int tableQue) {
         this.tableNO = tableNO;
         this.seats = seats;
         this.locationString = locationString;
+        this.tableQue = tableQue;
     }
     
 }
