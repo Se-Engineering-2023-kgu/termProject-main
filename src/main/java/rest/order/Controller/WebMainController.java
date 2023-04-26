3b68cd3f892702027aadb8f91ff1fb7bd0372f0b
choi4624 <choi4624@gmail.com>
2023-04-26 15:20:45 +0900

    html test

--- src/main/java/rest/order/Controller/WebMainController.java
+++ src/main/java/rest/order/Controller/WebMainController.java
@@ -1,29 +1,44 @@
 package rest.order.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.ui.Model;
+
 import rest.order.Model.Menu;
 import rest.order.Service.MenuService;
 
-
 @Controller
-@RequestMapping(path="/") 
 public class WebMainController {
     // @Autowired
-    // private 
+    // private
     MenuService mService;
 
     @GetMapping("/")
-	public String rootPage(HttpServletRequest request) {
-		return "greeting";
-	}
+    public String rootPage(Model model) {
+        model.addAttribute("greeting", "greeting");
+        return "greeting";
+    }
 
-    @GetMapping("/hello")
-	public String helloPage(HttpServletRequest request) {
+    @GetMapping("/user")
+    public String helloPage(Model model) {
+        model.addAttribute("userinfo", "thymeleaf");
         System.out.println(mService.menuName());
-        return "hello";
-	}
+        return "usertemp";
+    }
+
+    @GetMapping("/good")
+    public String goodreturn() {
+        return "index";
+    }
 
 }
