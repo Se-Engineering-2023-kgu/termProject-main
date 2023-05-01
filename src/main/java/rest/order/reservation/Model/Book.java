e9403f00b6f64c70b9346a6758314ffbff811ad2
choi4624 <choi4624@gmail.com>
2023-05-01 13:44:18 +0900

    menu에 대한 연결 완료

--- src/main/java/rest/order/reservation/Model/Book.java
+++ src/main/java/rest/order/reservation/Model/Book.java
@@ -8,33 +8,33 @@ import java.time.LocalTime;
 import java.util.List;
 
 /* 이거 그냥 엔티티 하면 오류남 */
-// @Entity
+@Entity
 // @Table(name = "bookinglist")
 public class Book {
-   // need to Declare Database structure
-   // how to Book dataBase using Userinfo or any other things;
-   // 해당 클래스를 위해선 정확한 데이터베이스 구조 선정이 중요합니다.
-   // 구조에 대해서 좀 더 정확한 무언가가 필요해요.
+    // need to Declare Database structure
+    // how to Book dataBase using Userinfo or any other things;
+    // 해당 클래스를 위해선 정확한 데이터베이스 구조 선정이 중요합니다.
+    // 구조에 대해서 좀 더 정확한 무언가가 필요해요.
 
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @Column(name = "booking")
-   int bookingID;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "booking")
+    int bookingID;
 
-   OrderMenu orderInfo;
-   TableList TableInfo;
-   Customer customerInfo;
+    // OrderMenu orderInfo;
+    // TableList TableInfo;
+    // Customer customerInfo;
 
     public Book() {
-    
+
     }
 
-    public Book(Integer bookingID, OrderMenu orderInfo, TableList tableInfo, Customer customerInfo) {
-        this.bookingID = bookingID;
-        this.orderInfo = orderInfo;
-        TableInfo = tableInfo;
-        this.customerInfo = customerInfo;
-    }
-
+    // public Book(Integer bookingID, OrderMenu orderInfo, TableList tableInfo,
+    // Customer customerInfo) {
+    // this.bookingID = bookingID;
+    // this.orderInfo = orderInfo;
+    // TableInfo = tableInfo;
+    // this.customerInfo = customerInfo;
+    // }
 
 }
