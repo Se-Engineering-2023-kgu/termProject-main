73affdbf041456da47a06b22de8c2f2965c48b41
jookbooin <3670lsh@naver.com>
2023-05-09 09:54:36 +0900

    23-05-09 로그인 먼저

--- src/main/java/rest/order/reservation/Controller/LoginController.java
+++ src/main/java/rest/order/reservation/Controller/LoginController.java
@@ -2,7 +2,9 @@ package rest.order.reservation.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -12,6 +14,13 @@ public class LoginController {
     @GetMapping("/login")
     public String loginPage() {
         return "login/loginPage";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam String loginId, @RequestParam String loginPwd) {
+        System.out.println("loginId = " + loginId);
+        System.out.println("loginPwd = " + loginPwd);
+        return "redirect:/";
     }
 
 //    @PostMapping("/login")
