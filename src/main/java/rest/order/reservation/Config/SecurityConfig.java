344c4aca0c16cf991abb5d6586b583e46098bdd3
choi4624 <choi4624@gmail.com>
2023-05-15 15:57:55 +0900

    경로 소소하게 수정

--- src/main/java/rest/order/reservation/Config/SecurityConfig.java
+++ src/main/java/rest/order/reservation/Config/SecurityConfig.java
@@ -42,9 +42,9 @@ public class SecurityConfig {
                 .loginPage("/login/login") // controller에 정의한 로그인 페이지 (강제로 이상한데 가도 이 페이지로 감)
                 .loginProcessingUrl("/login/login") // form submit해서 제출하는 로그인 페이지
                 .usernameParameter("loginId") // 타임리프 페이지에서 받을 http form 파라미터 (기본값 : username)
-                .passwordParameter("loginPwd") // pw 파라미터를 제출받음
-                .defaultSuccessUrl("/customer/", true) // 로그인 하면 id 페이지로 이동
-                .failureUrl("/customer/login?error=true") // 로그인 실패하면 error=true 파라미터 페이지로 나오게 하기 (다른 경로 가능)
+                .passwordParameter("loginPwd") // pw 파라미터를 제출받음 
+                .defaultSuccessUrl("/customer/", true) // 로그인 하면 id 페이지로 이동 >> 파라미터 만들어야 함
+                .failureUrl("/login/login?error=true") // 로그인 실패하면 error=true 파라미터 페이지로 나오게 하기 (다른 경로 가능)
                 .permitAll()
                 .and()
                 .logout()
