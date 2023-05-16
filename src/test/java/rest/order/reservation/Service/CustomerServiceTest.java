5f70b24ffbe6d3fb697f8e6c96987a7af573a736
Minkype <ktx1232@gmail.com>
2023-05-17 04:55:52 +0900

    Test는 되었으니 데이터베이스 확인 X

--- src/test/java/rest/order/reservation/Service/CustomerServiceTest.java
+++ src/test/java/rest/order/reservation/Service/CustomerServiceTest.java
@@ -1,3 +1,93 @@
+package rest.order.reservation.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import rest.order.reservation.DefineEnum.UserClass;
+import rest.order.reservation.Model.DTO.AppUser.AppUserDTO;
+import rest.order.reservation.Model.DTO.AppUser.UserRegistForm;
+import rest.order.reservation.Service.UserService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@SpringBootTest
+@Transactional
+public class CustomerServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void testFindUser() {
+        // Given
+        UserRegistForm userRegistForm = new UserRegistForm("john123", "password", "John Doe", UserClass.customer, "123456789", "john.doe@example.com");
+        Long userId = userService.addUser(userRegistForm);
+
+        // When
+        AppUserDTO appUserDTO = userService.findUser(userId);
+
+        // Then
+        assertEquals(userId, appUserDTO.uid());
+        assertEquals("john123", appUserDTO.loginId());
+        assertEquals("John Doe", appUserDTO.name());
+        assertEquals(UserClass.customer, appUserDTO.userType());
+        assertEquals("123456789", appUserDTO.phonNumber());
+        assertEquals("john.doe@example.com", appUserDTO.email());
+    }
+
+    @Test
+    public void testDeleteUser() {
+        // Given
+        UserRegistForm userRegistForm = new UserRegistForm("john123", "password", "John Doe", UserClass.customer, "123456789", "john.doe@example.com");
+        Long userId = userService.addUser(userRegistForm);
+
+        // When
+        userService.deleteUser(userId);
+
+        // Then
+        AppUserDTO appUserDTO = userService.findUser(userId);
+        assertNull(appUserDTO);
+    }
+
+    @Test
+    public void testModifyUser() {
+        // Given
+        UserRegistForm userRegistForm = new UserRegistForm("john123", "password", "John Doe", UserClass.customer, "123456789", "john.doe@example.com");
+        Long userId = userService.addUser(userRegistForm);
+
+        // When
+        AppUserDTO updatedUserDTO = new AppUserDTO(userId, "john123", "newpassword", "John Doe", UserClass.customer, "987654321", "john.doe@example.com");
+        userService.modify(userId, updatedUserDTO);
+
+        // Then
+        AppUserDTO appUserDTO = userService.findUser(userId);
+        assertEquals("newpassword", appUserDTO.loginPwd());
+        assertEquals("987654321", appUserDTO.phonNumber());
+    }
+
+    @Test
+    public void testLoginCheck() {
+        // Given
+        UserRegistForm userRegistForm = new UserRegistForm("john123", "password", "John Doe", UserClass.customer, "123456789", "john.doe@example.com");
+        Long userId = userService.addUser(userRegistForm);
+
+        // When
+        AppUserDTO appUserDTO = userService.loginCheck("john123", "password");
+
+        // Then
+        assertEquals(userId, appUserDTO.uid());
+        assertEquals("john123", appUserDTO.loginId());
+        assertEquals("John Doe", appUserDTO.name());
+        assertEquals(UserClass.customer, appUserDTO.userType());
+        assertEquals("123456789", appUserDTO.phonNumber());
+        assertEquals("john.doe@example.com", appUserDTO.email());
+    }
+}
+
 //package rest.order.reservation.Service;
 //
 //import org.junit.jupiter.api.Test;
