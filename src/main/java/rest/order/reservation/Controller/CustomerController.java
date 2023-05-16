e33fcac9937a288a9e5315e1b9f893d3c3d18496
Gyoung-0 <126968510+Gyoung-0@users.noreply.github.com>
2023-05-16 14:04:07 +0900

    경영

--- src/main/java/rest/order/reservation/Controller/CustomerController.java
+++ src/main/java/rest/order/reservation/Controller/CustomerController.java
@@ -1,4 +1,4 @@
-package rest.order.reservation.controller;
+package rest.order.reservation.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- src/main/java/rest/order/reservation/Repository/ReservationRepo.java
+++ src/main/java/rest/order/reservation/Repository/ReservationRepo.java
@@ -12,7 +12,7 @@ import rest.order.reservation.Model.Reservation;
  */
 
 
-//queryDsl을 사용하기위해서는 새로운 기능을 넣기위한 Custom 인터페이스와 그걸 구현한 ( 원래이름 : ReservationRepo ) + Impl 형식을 붙인 이름으로 구현후
+//queryDsl을 사용하기위해서는 새로운 기능을 넣기위한 Custom 인터    페이스와 그걸 구현한 ( 원래이름 : ReservationRepo ) + Impl 형식을 붙인 이름으로 구현후
 // 상속을 진행함 -> queryDsl을 사용하는 방식중에 하나입니다.
 @Repository
 public interface ReservationRepo extends JpaRepository<Reservation, Long>, ReservationRepoCustom {
