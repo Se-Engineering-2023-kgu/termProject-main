969510d1159feedbaa54dc1e5bcc937e2d00588d
jookbooin <3670lsh@naver.com>
2023-06-04 00:20:46 +0900

    23-06-03 사용자 예약 table button 막음

--- src/main/java/rest/order/reservation/Service/TableService.java
+++ src/main/java/rest/order/reservation/Service/TableService.java
@@ -2,20 +2,28 @@ package rest.order.reservation.Service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import rest.order.reservation.DefineEnum.TimeSlot;
 import rest.order.reservation.Model.DTO.TableDTO;
+import rest.order.reservation.Model.Reservation;
 import rest.order.reservation.Model.TableList;
+import rest.order.reservation.Repository.Reservation.ReservationRepo;
 import rest.order.reservation.Repository.TableRepo;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class TableService {
 
     TableRepo tableRepository;
+    ReservationRepo reservationRepository;
 
-    public TableService(TableRepo tableRepository) {
+    public TableService(TableRepo tableRepository, ReservationRepo reservationRepository) {
         this.tableRepository = tableRepository;
+        this.reservationRepository = reservationRepository;
     }
 
     public Long addTable(TableDTO tableDto) {
@@ -48,4 +56,18 @@ public class TableService {
     }
 
 
+    public List<Long> getAvailableTables(String date, String time) {
+        LocalDate findDate = LocalDate.parse(date);
+        System.out.println("findDate = " + findDate);
+        System.out.println("TimeSlot.valueOf(time) = " + TimeSlot.valueOf(time));
+        List<Reservation> reservationList = reservationRepository.findByDateSlotAndTimeSlot(findDate, TimeSlot.valueOf(time));
+
+        List<Long> tableLists = new ArrayList<>();
+        if (reservationList != null) {
+            tableLists = reservationList.stream()
+                    .map(reservation -> reservation.getTables().getTid())
+                    .collect(Collectors.toList());
+        }
+        return tableLists;
+    }
 }
